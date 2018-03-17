95962d67cbf09bf6aa429bb14902a53dbe1440ef
gobith <gkgobith@gmail.com>
2018-03-17 22:04:28 +0100

    Small fix

--- src/components/ScheduleColumn.vue
+++ src/components/ScheduleColumn.vue
@@ -1,15 +1,10 @@
 <template>
-<div>
   <td
     :colspan=column.colspan
     v-bind:style="{ backgroundColor: '#' + column.bgColor}"
     align="center"
     @click ="columnClicked">{{column.title}}
   </td>
-  <b-modal id="modal-center" centered title="Bootstrap-Vue">
-   <p class="my-4">Vertically centered modal!</p>
- </b-modal>
-  </div>
 </template>
 
 
